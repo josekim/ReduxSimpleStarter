3aaba33196aabceba9965c94ec2359ebce14a984
Joseph Kim <joseph.kim85@gmail.com>
2017-04-04 16:36:21 -0500

    Update after section 1
    Section 1 updates

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTsearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyCF33h9nTzEQtoOWaDq-ud4Nqxtxywbqjw';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+YTsearch({key:API_KEY, term: 'surfboards'}, function(data){
+  console.log(data);
+});
+
+// Create a new component.  This component should produce
+//some html
+const App = () => {
+  return(
+    <div>
+      <SearchBar />
+    </div>
+  );//JSX a dialect of javascript -
+    //produces the HTML that gets rendered
+
+}
+
+//  Take this component's generated HTML and put it
+// on the page(in the dom)
+ReactDOM.render(<App />, document.querySelector('.container'));
+//React.render(App); - Doesn't Work Need to the Libraryies
+// Core react Library know how to work with core COmponents
+// Funcationality to render to DOM is different Library - ReactDOM
+//We use ReactDom to render to DOM
+//ReactDom.render(App);
+//Instead of passing a class, please make sure to instantiate
+// need to instantiate or create a instance of Component.  <App />
+//ReactDom.render(<App />) Target container is not a DOM - we are not specifying
+// where it needs to Go.  2nd Argument to specify Existing Dom Node
+
+//downward data flow. The parent should be in charge of fetching data
