ef2ecb0c7038adb199ef9ac1dc98dbad5f35910c
Joseph Kim <joseph.kim85@gmail.com>
2017-04-05 14:18:52 -0500

    Section 2 Complete
    Youtube Searcher Complete

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,14 +9,21 @@ class SearchBar extends Component {// inherit from React.Component class
   }
   render() { // every Component must have a Render Method
     return (
-    <div>
+    <div className="search-bar">
       <input
         value = {this.state.term}
-        onChange={(event) => this.setState({term: event.target.value})}/>
+        onChange={event => this.onInputChange(event.target.value)}/>
     </div>
   );
   }
+  onInputChange(term){
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+
 }
+
+
 export default SearchBar;
 
 
--- src/index.js
+++ src/index.js
@@ -1,38 +1,50 @@
-import React from 'react';
+import _ from 'lodash';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTsearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCF33h9nTzEQtoOWaDq-ud4Nqxtxywbqjw';
 
-YTsearch({key:API_KEY, term: 'surfboards'}, function(data){
-  console.log(data);
-});
 
-// Create a new component.  This component should produce
-//some html
-const App = () => {
-  return(
-    <div>
-      <SearchBar />
-    </div>
-  );//JSX a dialect of javascript -
-    //produces the HTML that gets rendered
 
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos:[],
+      selectedVideo: null
+    };
+
+    this.videoSearch("surfboards");
+  }
+
+  videoSearch(term){
+    YTsearch({key:API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos:videos,
+        selectedVideo:videos[0]
+      });
+      // in es6 this.setState({videos:videos}); = this.setState({videos}) since property and value have same name
+    });
+  }
+  render(){
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)},300);
+
+
+    return  (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}
+          />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo =>this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
-//  Take this component's generated HTML and put it
-// on the page(in the dom)
 ReactDOM.render(<App />, document.querySelector('.container'));
-//React.render(App); - Doesn't Work Need to the Libraryies
-// Core react Library know how to work with core COmponents
-// Funcationality to render to DOM is different Library - ReactDOM
-//We use ReactDom to render to DOM
-//ReactDom.render(App);
-//Instead of passing a class, please make sure to instantiate
-// need to instantiate or create a instance of Component.  <App />
-//ReactDom.render(<App />) Target container is not a DOM - we are not specifying
-// where it needs to Go.  2nd Argument to specify Existing Dom Node
-
-//downward data flow. The parent should be in charge of fetching data
